3dbe94d74021483f16d7f715bdf22c7146a0d340
Lucas Mantovani <lrmantovani@fb.com>
2022-07-20 10:28:13 -0700

    Nice logo for not found

--- bizwiz-ui/src/components/Message/Message.jsx
+++ bizwiz-ui/src/components/Message/Message.jsx
@@ -1,10 +1,14 @@
 import "./Message.css";
+import Glasses from "./Glasses.png"
 export default function Message(props) {
   return (
     <div className="mainMessage">
       {props.message
         ? props.message
         : "There was an error loading the page. Please try again!"}
+      {(props.message && !props.message.includes("Loading..."))?
+        <img id="glassImage" src={Glasses}/>:
+        <></>}    
     </div>
   );
 }
